723326e366330d2c9b1f87ffb2c0066139fd308e
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-02-25 16:55:26 -0300

    Conclui requisito 4.

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const email = document.querySelector('#email-login');
 const password = document.querySelector('#password');
 
 function alertLogin() {
-    if(email.value === "tryber@teste.com" && password.value === "123456") {
-        alert("Olá, Tryber!")
-    } else {
-        alert("Email ou senha inválidos.")
-    }
+  if(email.value === "tryber@teste.com" && password.value === "123456") {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos.");
+  }
 }
 
-buttonLogin.addEventListener('click', alertLogin)
+buttonLogin.addEventListener('click', alertLogin);
