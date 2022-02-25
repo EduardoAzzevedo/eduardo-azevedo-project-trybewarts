d5ad3d38ec5b709e9cf1f9b457f8923a13e59a66
amandapccs <amandchen@hotmail.com>
2022-02-25 16:43:54 -0300

    Realizamos requisitos 1 - 3

---
+++ script.js
@@ -0,0 +1,14 @@
+// Variáveis
+const buttonLogin = document.querySelector('#btn-login');
+const email = document.querySelector('#email-login');
+const password = document.querySelector('#password');
+
+function alertLogin() {
+    if(email.value === "tryber@teste.com" && password.value === "123456") {
+        alert("Olá, Tryber!")
+    } else {
+        alert("Email ou senha inválidos.")
+    }
+}
+
+buttonLogin.addEventListener('click', alertLogin)
