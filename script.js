1fc80fe9613f3c69ec7c782af272a4498777139b
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-02-26 09:54:23 -0300

    conclui requisito 18.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const buttonLogin = document.querySelector('#btn-login');
 const email = document.querySelector('#email-login');
 const password = document.querySelector('#password');
+const sendButton = document.querySelector('#submit-btn');
+const checkBoxAgree = document.querySelector('#agreement');
+sendButton.disabled = true;
 
 function alertLogin() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -12,3 +15,13 @@ function alertLogin() {
 }
 
 buttonLogin.addEventListener('click', alertLogin);
+
+function disabled(_event) {
+  if (checkBoxAgree.checked) {
+    sendButton.disabled = false;
+  } else {
+    sendButton.disabled = true;
+  }
+}
+
+checkBoxAgree.addEventListener('click', disabled);
