4bffa9914eb6e7f8cb88d5772bb22b964dc71d45
amandapccs <amandchen@hotmail.com>
2022-02-26 11:02:28 -0300

    Realizamos requisito 20

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const email = document.querySelector('#email-login');
 const password = document.querySelector('#password');
 const sendButton = document.querySelector('#submit-btn');
 const checkBoxAgree = document.querySelector('#agreement');
+const textarea = document.querySelector('#textarea');
+const counter = document.querySelector('#counter');
 sendButton.disabled = true;
+counter.innerHTML = `Máximo de caracteres: ${500}`;
 
 function alertLogin() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -25,3 +28,11 @@ function disabled(_event) {
 }
 
 checkBoxAgree.addEventListener('click', disabled);
+
+// Referência: https://www.youtube.com/watch?v=LiQcxaC2Ek4&ab_channel=WebdesignemFoco
+function textareaCounter(event) {
+  const sub = event.target.maxLength - event.target.textLength;
+  counter.innerHTML = `Máximo de caracteres: ${sub}`;
+}
+
+textarea.addEventListener('keyup', textareaCounter);
